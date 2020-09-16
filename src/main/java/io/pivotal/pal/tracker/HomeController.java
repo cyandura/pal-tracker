0f61f69b1cc9c91c7b2136eb46c0db731d033578
Ubuntu <ubuntu@ip-172-31-95-185.ec2.internal>
2020-09-16 20:32:31 +0000

    Added a display of the entries list on the index page

--- src/main/java/io/pivotal/pal/tracker/HomeController.java
+++ src/main/java/io/pivotal/pal/tracker/HomeController.java
@@ -1,9 +1,12 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -12,9 +15,18 @@ public class HomeController {
     @Value("${memory.limit:NOT SET}") String memoryLimit;
     @Value("${cf.instance.addr:NOT SET}") String instanceAddr;
 
+
+    @Autowired
+    TimeEntryController timeEntryController;
+
     @RequestMapping("/index.html")
     @ResponseBody
     public String get(){
+        String entryString = "";
+        List<TimeEntry> entries = timeEntryController.list().getBody();
+        for(TimeEntry entry : entries){
+            entryString += entry.toString() + "<br>";
+        }
         return "<html>" +
                     "<h2>Welcome to Smart Impala</h2>" +
                     "<div>This is Cody Yandura's training class page \n Here's some Environment Variables</div> " +
@@ -22,6 +34,10 @@ public class HomeController {
                         "<li>Port: " + port + "</li>" +
                         "<li>Memory Limit: " + memoryLimit + "</li>" +
                         "<li>Instance Address: " + instanceAddr + "</li>" +
+                    "</ul>" +
+                    "<h3>Contents of the Time Entry List:</h3>" +
+                    "<ul>" +
+                        "<li>" + entryString + "</li>" +
                     "</ul>" +
                 "</html>";
     }
